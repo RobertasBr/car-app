0426d776580581a9cc7cd3696ca0784d931760dc
RobertasBr <123648407+RobertasBr@users.noreply.github.com>
2024-05-14 23:04:40 +0100

    working

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
+  providers: [provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"carenginelookup","appId":"1:157315191715:web:4ae7236f28fa3df79c8016","storageBucket":"carenginelookup.appspot.com","apiKey":"AIzaSyDt0SwFDeOnF8h_TRlYKbr_NWvZZ9nqzG8","authDomain":"carenginelookup.firebaseapp.com","messagingSenderId":"157315191715"})), provideFirestore(() => getFirestore())]
 }).catch(err => console.error(err));
